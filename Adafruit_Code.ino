113eebf832d9346903e6d70494857a8cbe0d36c8
HappyFeet3334 <ibtyler2@gmail.com>
2023-09-13 09:22:09 -0400

    edited to work with adafruit gemma V2 instead of tinker cad arduino uno

--- Adafruit_Code.ino
+++ Adafruit_Code.ino
@@ -3,41 +3,18 @@
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define LED_PIN 11
+#define LED_PIN 0
 #define NUM_LED 12
-#define BUTTON_PIN 2
-
-boolean oldState = HIGH;
-boolean newState = LOW;
 
 Adafruit_NeoPixel strip(NUM_LED, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup(){
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
   strip.begin();
   strip.show();
-  Serial.begin(9600);
 }
 
 void loop(){
-  newState = digitalRead(BUTTON_PIN);
   
-  if((newState==LOW)&&(oldState==HIGH)){
-    delay(10);
-    newState=digitalRead(BUTTON_PIN);
-    int random_num = random(0,3);
-    if(random_num == 0){
-      loading_color(strip.Color(0,0,255),50);
-      Serial.print(random_num);
-    }else if(random_num == 1){
-      clap(strip.Color(0,255,0),50);
-      Serial.print(random_num);
-    }else{
-      running(strip.Color(255,0,0));
-      Serial.print(random_num);
-    }
-  }
-  oldState=newState;
 }
 
 void loading_color(uint32_t color, int wait){
