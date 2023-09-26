5ee3c086442afaf1080f0c9d0b867b29253f44ba
HappyFeet3334 <ibtyler2@gmail.com>
2023-09-26 02:55:30 -0400

    Seprated functions and put them in a new file

--- Adafruit_Code.ino
+++ Adafruit_Code.ino
@@ -1,7 +1,9 @@
 #include <Adafruit_NeoPixel.h>
+#include<Adafruit_Code.ino>
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
+
 
 #define LED_PIN 0
 #define NUM_LED 12
@@ -14,57 +16,4 @@ void setup(){
 
 void loop(){
   
-}
-
-void loading_color(uint32_t color, int wait){
-  for(int i=0;i<NUM_LED;i++){
-    strip.setPixelColor(i,color);
-    strip.show();
-    delay(wait);
-  }
-  for(int i=0;i<NUM_LED;i++){
-    strip.setPixelColor(i,strip.Color(  0,   0,   0));
-    strip.show();
-    delay(wait);
-  }
-}
-
-void clap(uint32_t color, int wait){
-  for(int i=0;i<NUM_LED/2;i++){
-    strip.setPixelColor(i,color);
-    strip.setPixelColor(12-i,color);
-    strip.show();
-    delay(wait);
-  }
-  strip.setPixelColor(6,color);
-  strip.show();
-  delay(wait);
-  for(int i=0;i<NUM_LED/2;i++){
-    strip.setPixelColor(6-i,strip.Color(0,0,0));
-    strip.setPixelColor(6+i,strip.Color(0,0,0));
-    strip.show();
-    delay(wait);
-  }
-  strip.setPixelColor(0,strip.Color(0,0,0));
-  strip.show();
-}
-
-void running(uint32_t color){
-  for(int i=0;i<NUM_LED;i++){
-    strip.setPixelColor(i,color);
-    strip.show();
-    delay(50);
-    strip.setPixelColor(i+1,color);
-    strip.setPixelColor(i-1,strip.Color(0,0,0));
-    strip.show();
-  }
-  strip.setPixelColor(11,strip.Color(0,0,0));
-  strip.show();
-}
-
-void test0(uint32_t color){
-  for(int i=0;i<NUM_LED;i++){
-    strip.setPixelColor(i,color);
-    strip.show();
-  }
 }
