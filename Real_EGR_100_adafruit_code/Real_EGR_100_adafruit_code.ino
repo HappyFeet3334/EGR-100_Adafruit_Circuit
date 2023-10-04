e57ec17ca386ebaf83a1de14f480df73fbf51db5
HappyFeet3334 <ibtyler2@gmail.com>
2023-10-03 22:44:33 -0400

    // added comments and cleaned up the code

--- Real_EGR_100_adafruit_code/Real_EGR_100_adafruit_code.ino
+++ Real_EGR_100_adafruit_code/Real_EGR_100_adafruit_code.ino
@@ -1,38 +1,13 @@
-// NeoPixel test program showing use of the WHITE channel for RGBW
-// pixels only (won't look correct on regular RGB NeoPixel strips).
+#include "global.h"
 
-#include "led_functions.h"
-
-//#include <Adafruit_NeoPixel.h>
-//#ifdef __AVR__
-// #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-//#endif
-
-//// Which pin on the Arduino is connected to the NeoPixels?
-//// On a Trinket or Gemma we suggest changing this to 1:
-//#define LED_PIN     0
-//
-//// How many NeoPixels are attached to the Arduino?
-//#define LED_COUNT  16
-//
-//// NeoPixel brightness, 0 (min) to 255 (max)
-//#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
-//
-//// Declare our NeoPixel strip object:
-//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
-
-
-//#ifdef __AVR__
-// #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-//#endif
-
+//Runs onces before void loop
 void setup() {
-  strip.begin();           
-  strip.show();            
-  strip.setBrightness(BRIGHTNESS);
+  strip.begin();     //Intailizes the Neo pixel      
+  strip.show();            //Needed to display LEDs 
+  strip.setBrightness(BRIGHTNESS);      //sets the brightness from 1-255
 }
-
+//loops code inside 
 void loop() {
-  //colorWipe(strip.Color(255,   0,   0)     , 50); // Red
-  loading(strip.Color(0,0,255)    ,50);
+  //colorWipe(strip.Color(255,   0,   0)     , 50); 
+  loading(strip.Color(0,0,255)    ,50);     //Calls the loading LED pattern from led_functions.ino
 }
--- Real_EGR_100_adafruit_code/led_functions.ino
+++ Real_EGR_100_adafruit_code/led_functions.ino
@@ -1,11 +1,12 @@
-#include "led_functions.h"
+#include "global.h"    //Includes global varaibles and functions
 
+//LED pattern that does the windows loading dots 
 void loading(uint32_t color, int wait) 
 {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
+  for(int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);         
+    strip.show();                          
+    delay(wait);                           
   }
   for(int i=0;i<strip.numPixels();i++){
     strip.setPixelColor(i,strip.Color(  0,   0,   0));
@@ -13,7 +14,7 @@ void loading(uint32_t color, int wait)
     delay(wait);
   }
 }
-
+//LED patern that changes all the LEDs to a uniform color by circling around
 void colorwipe(uint32_t color)
 {
   for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
@@ -21,7 +22,7 @@ void colorwipe(uint32_t color)
   }
   strip.show();
 }
-
+//turns on LEDs while and meets half way and then turn off
 void clap(uint32_t color, int wait)
 {
   for(int i=0;i<strip.numPixels()/2;i++){
@@ -42,7 +43,7 @@ void clap(uint32_t color, int wait)
   strip.setPixelColor(0,strip.Color(0,0,0));
   strip.show();
 }
-
+//Turns on two LEDs and goes in a cricle around the Neo Pixel
 void run2(uint32_t color)
 {
   for(int i=0;i<strip.numPixels();i++){
